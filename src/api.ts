948b93489a8d9319fad0a512eeda40f0e114c818
Matthieu Aubry <ithasu@gmail.com>
2023-04-14 23:35:07 +0200

    Queue

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,15 @@
-export type SamplingMethod = "Euler a" | "Euler" | "LMS" | "Heun" | "DPM2" | "DPM2 a" | "DPM++ S2 a" | "DPM++ 2M" | "DPM++ SDE" | "DPM fast" | "DPM adaptive" | "LMS Karras" | "DPM2 Karras" | "DPM2 a Karras" | "DPM++ 2S a Karras" | "DPM++ 2M Karras" | "DPM++ SDE Karras" | "DDIM" | "PLMS";
+import { writeFileSync } from "node:fs";
+import { join } from "node:path";
 
-
+export type SamplingMethod =
+    "Euler a" | "Euler" |
+    "LMS" | "Heun" |
+    "DPM2" | "DPM2 a" | "DPM++ S2 a" | "DPM++ 2M" | "DPM++ SDE" | "DPM fast" | "DPM adaptive" |
+    "LMS Karras" |
+    "DPM2 Karras" | "DPM2 a Karras" |
+    "DPM++ 2S a Karras" | "DPM++ 2M Karras" | "DPM++ SDE Karras" |
+    "DDIM" |
+    "PLMS";
 
 export interface Txt2ImgOptions {
     prompt: string;
@@ -35,4 +44,44 @@ export async function txt2img(apiUrl: string, options: Txt2ImgOptions) {
         }
         return data;
     }
+}
+
+export async function queue(apiUrl: string, output_folder: string, options: {
+    prompts: { positive: string, negative?: string }[],
+    seeds: number[]
+}): Promise<void> {
+    const inputs: Txt2ImgOptions[] = [];
+    const default_parameters: Txt2ImgOptions = {
+        prompt: "",
+        negative_prompt: "",
+        seed: -1,
+
+        width: 512,
+        height: 512,
+        steps: 20,
+        sampler_name: "DPM++ 2M",
+
+        n_iter: 1,
+        batch_size: 1,
+        cfg_scale: 7,
+    };
+
+    for (const prompt of options.prompts) {
+        for (const seed of options.seeds) {
+            inputs.push({
+                ...default_parameters,
+                prompt: prompt.positive,
+                negative_prompt: prompt.negative,
+                seed,
+            });
+        }
+    }
+
+    let i = 0;
+    const result = inputs.map((options => {
+        return txt2img(apiUrl, options).then((data) => {
+            writeFileSync(join(output_folder, `images-${i++}.png`), data.images[0], "base64");
+        });
+    }));
+    await Promise.all(result).then(() => { });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,17 @@
-import { writeFileSync } from "node:fs";
-import { txt2img } from "./api";
+import { queue } from "./api";
 
 async function main() {
     const apiUrl = "http://192.168.42.20:7860";
 
     try {
-        const { images } = await txt2img(apiUrl, {
-            prompt: 'A meteorite falling on earth',
-            negative_prompt: 'bad anatomy',
-            sampler_name: "DPM++ 2M",
-            width: 512,
-            height: 512,
-            steps: 20,
-            batch_size: 1,
-            n_iter: 1,
-            cfg_scale: 7,
-            seed: 46012
+        await queue(apiUrl, "./images/", {
+            prompts: [
+                { positive: "A meteorite falling on the earth", negative: "human" },
+                { positive: "A meteorite falling on a planet" },
+                { positive: "A meteorite falling on a planet, wide angle view" }
+            ],
+            seeds: [46012, 123456, 6546435435]
         });
-
-        images.forEach((image, i) =>
-            writeFileSync(`image-${i}.png`, images[i], 'base64')
-        )
     } catch (err) {
         console.error(err)
     }
