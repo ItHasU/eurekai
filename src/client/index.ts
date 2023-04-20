c416be9cd39c0c3c16322673a9019f6dd2cdace0
Matthieu Aubry <ithasu@gmail.com>
2023-04-20 07:09:36 +0200

    Connect db between client and server

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,15 +1,19 @@
-console.log("Init...");
+async function main(): Promise<void> {
+    var db = new PouchDB('pictures');
+    db.replicate.to('/db');
+    db.replicate.from('/db');
+    
+    db.post({
+      name: 'David',
+      age: 69
+    });
+        
+    const docs = await db.allDocs({
+        include_docs: true,
+        attachments: true
+    });
+    
+    console.log(JSON.stringify(docs));
+}
 
-var db = new PouchDB('pictures');
-
-db.put({
-  _id: 'dave@gmail.com',
-  name: 'David',
-  age: 69
-});
-
-db.changes().on('change', function() {
-  console.log('Ch-Ch-Changes');
-});
-
-db.replicate.to('/db');
+main().catch(e => console.error(e));
