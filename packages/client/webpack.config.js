8ddd8d2fd563abed83f7cdcc395891593a49651d
Matthieu Aubry <ithasu@gmail.com>
2023-04-21 23:44:18 +0200

    WIP : Not working

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './build/index.js',
+  entry: './build/src/index.js',
   mode: "production",
   plugins: [
     new HtmlWebpackPlugin({
