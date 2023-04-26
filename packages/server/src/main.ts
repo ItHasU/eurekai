1ca0a96a48926d469e9d7cbb045c32d5d32a7c4c
Matthieu Aubry <ithasu@gmail.com>
2023-04-26 12:39:15 +0200

    Remove obsolete files

--- packages/server/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { ServerDBConnector } from "./modules/serverDB";
-import { init } from "./modules/server";
-
-const apiUrl = "http://localhost:7860/";
-
-const app = init({
-    port: 5001
-});
-const db = new ServerDBConnector(apiUrl);
