dc73a939dad38e05651ceaadac0b5b30c762bb59
Matthieu Aubry <ithasu@gmail.com>
2023-05-10 13:22:43 +0200

    Give up on arm v7 for now

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd packages/node-sqlite3-master;npm install --build-from-source;cd -
 cd packages/shared;npm install;cd -
 cd apps/server;npm install;cd -
 cd apps/client;npm install;cd -
+npm install
