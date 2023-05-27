8abdb415ce9395bf1045212a9b565ef37bc4f4d0
Matthieu Aubry <aubry@cervval.com>
2023-05-27 23:07:23 +0200

    Generate new image before highres

--- apps/server/src/modules/generator.ts
+++ apps/server/src/modules/generator.ts
@@ -15,6 +15,36 @@ export class Generator {
     }
 
     protected async _unqueue(): Promise<boolean> {
+        // -- Get active prompts --
+        const prompts = await this._data.getPendingPrompts();
+        if (prompts.length !== 0) {
+            console.debug(`${prompts.length} active prompt(s)`);
+
+            // -- Get the prompt with the less pictures --
+            let firstPrompt = prompts[0];
+            if (firstPrompt) {
+                console.log(`Selected #${firstPrompt.orderIndex}, ${firstPrompt.pendingPictureCount}/${firstPrompt.bufferSize} pending, ${firstPrompt.acceptedPictureCount}/${firstPrompt.acceptedTarget} accepted`);
+
+                // -- Check if there is a preferred seed pending --
+                const preferredSeed = await this._data.getSeedPending(firstPrompt.id);
+
+                const picture = await this._data.createPictureFromPrompt(firstPrompt, preferredSeed ?? undefined);
+                const apiUrl = this._apiUrl;
+                console.debug(`Requesting a new image on ${apiUrl}...`);
+                try {
+                    const images = await txt2img(apiUrl, picture.options);
+                    console.debug(`${images.length} image(s) received`);
+                    if (images.length > 0) {
+                        await this._data.setPictureData(picture.id, images[0]);
+                    }
+                } catch (err) {
+                    console.error(err);
+                    await this._data.setPictureStatus(picture.id, ComputationStatus.ERROR);
+                }
+                return true;
+            }
+        }
+
         // -- Get pending highres pictures --
         const highresPictures = await this._data.getPicturesHighresPending();
         if (highresPictures.length > 0) {
@@ -46,44 +76,12 @@ export class Generator {
                 // Make sure we won't try again
                 await this._data.setPictureHighresStatus(picture.id, HighresStatus.ERROR);
             }
-        }
-
-        // -- Get active prompts --
-        const prompts = await this._data.getPendingPrompts();
-        if (prompts.length === 0) {
-            // Nothing left to generate
-            console.debug("No active prompt")
-            return false;
-        }
-        console.debug(`${prompts.length} active prompt(s)`);
-
-        // -- Get the prompt with the less pictures --
-        let firstPrompt = prompts[0];
-        if (!firstPrompt) {
-            console.log("No active prompt pending");
-            return false;
-        } else {
-
-            console.log(`Selected #${firstPrompt.orderIndex}, ${firstPrompt.pendingPictureCount}/${firstPrompt.bufferSize} pending, ${firstPrompt.acceptedPictureCount}/${firstPrompt.acceptedTarget} accepted`);
-
-            // -- Check if there is a preferred seed pending --
-            const preferredSeed = await this._data.getSeedPending(firstPrompt.id);
-
-            const picture = await this._data.createPictureFromPrompt(firstPrompt, preferredSeed ?? undefined);
-            const apiUrl = this._apiUrl;
-            console.debug(`Requesting a new image on ${apiUrl}...`);
-            try {
-                const images = await txt2img(apiUrl, picture.options);
-                console.debug(`${images.length} image(s) received`);
-                if (images.length > 0) {
-                    await this._data.setPictureData(picture.id, images[0]);
-                }
-            } catch (err) {
-                console.error(err);
-                await this._data.setPictureStatus(picture.id, ComputationStatus.ERROR);
-            }
             return true;
         }
+
+        // -- Nothing was generated --
+        console.debug("Nothing to generate");
+        return false;
     }
 
     protected async _scheduleNextIfNeeded(): Promise<void> {
