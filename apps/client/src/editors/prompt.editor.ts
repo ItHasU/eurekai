5ac22ed9bc0fd0b56e8daa6570ffd534ec961391
Matthieu Aubry <aubry@cervval.com>
2023-11-03 22:56:22 +0100

    Width/Height inputs

--- apps/client/src/editors/prompt.editor.ts
+++ apps/client/src/editors/prompt.editor.ts
@@ -5,6 +5,8 @@ export class PromptEditor extends HTMLElement {
     protected readonly _positiveInput: HTMLInputElement;
     protected readonly _negativeInput: HTMLInputElement;
     protected readonly _bufferSizeInput: HTMLInputElement;
+    protected readonly _widthInput: HTMLInputElement;
+    protected readonly _heightInput: HTMLInputElement;
 
     constructor() {
         super();
@@ -13,6 +15,8 @@ export class PromptEditor extends HTMLElement {
         this._positiveInput = this.querySelector("#positiveInput") as HTMLInputElement;
         this._negativeInput = this.querySelector("#negativeInput") as HTMLInputElement;
         this._bufferSizeInput = this.querySelector("#bufferSizeInput") as HTMLInputElement;
+        this._widthInput = this.querySelector("#widthInput") as HTMLInputElement;
+        this._heightInput = this.querySelector("#heightInput") as HTMLInputElement;
     }
 
     public setPrompt(prompt?: PromptDTO): void {
@@ -20,6 +24,8 @@ export class PromptEditor extends HTMLElement {
         this._positiveInput.value = prompt?.prompt ?? "";
         this._negativeInput.value = prompt?.negative_prompt ?? "";
         this._bufferSizeInput.value = "" + (prompt?.bufferSize ?? 10);
+        this._widthInput.value = "" + (prompt?.width ?? 1024);
+        this._heightInput.value = "" + (prompt?.height ?? 1024);
     }
 
     public getPrompt(): Omit<PromptDTO, "id" | "projectId" | "orderIndex" | "active"> {
@@ -27,6 +33,8 @@ export class PromptEditor extends HTMLElement {
         const positivePrompt = this._positiveInput.value;
         const negativePrompt = this._negativeInput.value;
         const bufferSize = +this._bufferSizeInput.value;
+        const width = +this._widthInput.value;
+        const height = +this._heightInput.value;
 
         // -- Build object --
         return {
@@ -34,8 +42,8 @@ export class PromptEditor extends HTMLElement {
             negative_prompt: negativePrompt ? negativePrompt : undefined,
             bufferSize,
 
-            width: 1024,
-            height: 1024,
+            width,
+            height,
             model: "SDXL juggernautXL_version6Rundiffusion.safetensors [1fe6c7ec54] + sd_xl_refiner_1.0_0.9vae.safetensors [8d0ce6c016]"
         };
     }
