62dbd26700ad620f6ef31ddafca61ad8ae0668b2
Matthieu Aubry <aubry@cervval.com>
2023-12-28 21:47:28 +0100

    WIP : Typings

--- dagda/tests/pg/src/main.ts
+++ dagda/tests/pg/src/main.ts
@@ -1,41 +1,151 @@
-import { PGRunner } from "@dagda/server/sql/impl/pg.runner";
-import { SQLConnection } from "@dagda/server/sql/runner";
-import { getEnvString } from "@dagda/server/tools/config";
-import { BaseDTO } from "@dagda/shared/sql/types";
+import { JSStaticType, JSTypes } from "@dagda/shared/typings/javascript.types";
 
-interface UserDTO extends BaseDTO {
-    id: number;
-    name: string;
+export class TypeHandler<
+    DBFieldTypes extends string,
+    Types extends Record<string, TypeDefinition<JSTypes, JSTypes, DBFieldTypes>>,
+    Tables extends Record<string, Record<string, keyof Types>>
+> {
+
+    constructor(protected readonly _types: Types, protected readonly _tables: Tables) {
+    }
+
+    public static type<JSType extends JSTypes, DBType extends JSTypes, DBTypeName extends string>(definition: TypeDefinition<JSType, DBType, DBTypeName>): TypeDefinition<JSType, DBType, DBTypeName> {
+        return definition;
+    }
+
+    public get types(): { [K in keyof Types]: NamedType<K, Types[K]> } {
+        return undefined as any;
+    }
+
+    /** Use this property with typeof to get the list */
+    public get tablenames(): keyof Tables {
+        return undefined as any;
+    }
+
+    /** Use this property to get the list of types associated to table names */
+    public get dtos(): { [Table in keyof Tables]: { [K in keyof Tables[Table]]: NamedType<Tables[Table][K], Types[Tables[Table][K]]> } } {
+        return undefined as any;
+    }
+
+    /** Get the list of tables */
+    public getTables(): (keyof Tables)[] {
+        return Object.keys(this._tables);
+    }
+
+    /** Get the list of fields for a table */
+    public getTableFields<T extends keyof Tables>(table: T): (keyof Tables[T])[] {
+        return Object.keys(this._tables[table]);
+    }
+
+    public getFieldType<T extends keyof Tables, F extends keyof Tables[T]>(table: T, field: F): Types[Tables[T][F]] {
+        return this._types[this._tables[table][field]];
+    }
+
 }
 
-type Tables = {
-    users: UserDTO;
+export class PGTypeHandler<
+    Types extends Record<string, TypeDefinition<JSTypes, JSTypes, PGTypes>>,
+    Tables extends Record<string, Record<string, keyof Types>>
+> extends TypeHandler<PGTypes, Types, Tables> {
+}
+
+type TypeDefinition<JSType extends JSTypes, DBType extends JSTypes, DBTypeNames extends string> = {
+    /** The type of the value when stored in JS */
+    rawType: JSType,
+    /** The type of the value when stored in the DB */
+    dbType: DBType,
+    /** The type name for the DB */
+    dbTypeName: DBTypeNames,
+    /** Convert from RAW to DB */
+    store?: (v: JSStaticType<JSType>) => JSStaticType<DBType>,
+    /** Convert from DB to RAW */
+    restore?: (v: JSStaticType<DBType>) => JSStaticType<JSType>
+}
+
+type PGTypes = "SERIAL" | "TEXT" | "BOOLEAN" | "INTEGER" | "BIGINT";
+
+type Named<Name, Value> = Value & { _name: Name };
+type NamedType<Name, T> = T extends TypeDefinition<infer JSType, any, string> ? Named<Name, JSStaticType<JSType>> : never;
+
+const APP_TYPES = new PGTypeHandler({
+    "PROJECT_ID": {
+        rawType: JSTypes.number,
+        dbType: JSTypes.string, // As it is stored as a BigInt, we will retrieve it as a string
+        dbTypeName: "SERIAL"
+    },
+    "PROMPT_ID": {
+        rawType: JSTypes.number,
+        dbType: JSTypes.string, // As it is stored as a BigInt, we will retrieve it as a string
+        dbTypeName: "SERIAL"
+    },
+    "TEXT": {
+        rawType: JSTypes.string,
+        dbType: JSTypes.string,
+        dbTypeName: "TEXT"
+    },
+    "BOOLEAN": {
+        rawType: JSTypes.boolean,
+        dbType: JSTypes.boolean,
+        dbTypeName: "BOOLEAN"
+    },
+    "INTEGER": {
+        rawType: JSTypes.number,
+        dbType: JSTypes.number,
+        dbTypeName: "INTEGER"
+    },
+    "SEED": PGTypeHandler.type({
+        rawType: JSTypes.number,
+        dbType: JSTypes.string,
+        dbTypeName: "BIGINT",
+        store: (v: number) => String(v),
+        restore: (v: string) => parseInt(v)
+    })
+}, {
+    projects: {
+        id: "PROJECT_ID",
+        name: "TEXT"
+    },
+    prompts: {
+        id: "PROMPT_ID",
+        projectId: "PROJECT_ID",
+        prompt: "TEXT",
+        negativePrompt: "TEXT"
+    }
+});
+
+function asNamed<Name extends string, V>(v: V): Named<Name, V> {
+    return v as Named<Name, V>;
+}
+
+interface Tables {
+    "projects": ProjectDTO
+}
+
+type AppTypes = typeof APP_TYPES.types;
+type AppTableNames = typeof APP_TYPES.tablenames;
+type AppDTOs = typeof APP_TYPES.dtos;
+
+type ProjectDTO = AppDTOs["projects"];
+type PromptDTO = AppDTOs["prompts"];
+
+const project: ProjectDTO = {
+    id: asNamed(1),
+    name: asNamed("test")
+};
+const prompt: PromptDTO = {
+    id: asNamed(1),
+    projectId: project.id,
+    prompt: asNamed("test"),
+    negativePrompt: asNamed("test")
 };
 
-
-async function _main(): Promise<void> {
-    const connectionString = getEnvString("DATABASE_URL");
-    console.log("Connecting to database...");
-    const runner = new PGRunner<Tables>({
-        users: {
-            name: false
-        }
-    }, connectionString);
-
-    await runner.initTable("users", {
-        name: "TEXT NOT NULL"
-    });
-
-    console.log("Connected to database");
-
-    await runner.withTransaction(async (connection: SQLConnection) => {
-        try {
-            await connection.run("CREATE SCHEMA IF NOT EXISTS test");
-        } catch (e) {
-            // Ignore error
-            console.log(`Schema creation failed: ${e}`);
-        }
-    });
+const tables = APP_TYPES.getTables();
+console.log(tables);
+for (const table of APP_TYPES.getTables()) {
+    const fields = APP_TYPES.getTableFields(table);
+    console.log(table, ":", fields);
+    for (const field of fields) {
+        const type = APP_TYPES.getFieldType(table, field);
+        console.log(`${table}.${field}: ${type.dbTypeName}`);
+    }
 }
-
-_main().catch(console.error.bind(console));
