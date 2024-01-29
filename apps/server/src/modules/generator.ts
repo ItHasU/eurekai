270d0fc3f1395b34b51a21a2ba7f8876ea228445
Matthieu Aubry <aubry@cervval.com>
2024-01-29 21:48:50 +0100

    Refactor generator

--- apps/server/src/modules/generator.ts
+++ apps/server/src/modules/generator.ts
@@ -6,16 +6,11 @@ import { ImageDescription } from "src/diffusers/diffuser";
 import { buildServerEntitiesHandler } from "./entities.handler";
 
 export class Generator {
-    protected _stopOnNextTimeout: boolean = false;
     protected _handler: ReturnType<typeof buildServerEntitiesHandler>;
 
     constructor(protected _db: AbstractSQLRunner<any, any>) {
         this._handler = buildServerEntitiesHandler(this._db);
-        this._scheduleNextIfNeeded();
-    }
-
-    public stop(): void {
-        this._stopOnNextTimeout = true;
+        this._unqueue();
     }
 
     protected async _unqueue(): Promise<boolean> {
@@ -26,57 +21,56 @@ export class Generator {
 
             // -- Handle pending lowres pictures --
             const picturesPending = this._handler.getItems("pictures"); // No need to filter, we only fetch the pending ones with the selected context
-            picturesPending.sort((p1, pZ) => {
+            picturesPending.sort((p1, p2) => {
                 let res: number = 0;
                 if (res === 0) {
                     // Compare by model to limit the switch of diffuser
                     const prompt1 = this._handler.getById("prompts", p1.promptId);
-                    const promptZ = this._handler.getById("prompts", pZ.promptId);
-                    if (prompt1 != null && promptZ != null) {
-                        res = prompt1.model.localeCompare(promptZ.model);
+                    const prompt2 = this._handler.getById("prompts", p2.promptId);
+                    if (prompt1 != null && prompt2 != null) {
+                        res = prompt1.model.localeCompare(prompt2.model);
                     }
                 }
 
                 if (res === 0) {
                     // Render lowres images first
                     const lowres1 = p1.status === ComputationStatus.PENDING ? 0 : 1;
-                    const lowresZ = pZ.status === ComputationStatus.PENDING ? 0 : 1;
+                    const lowres2 = p2.status === ComputationStatus.PENDING ? 0 : 1;
 
-                    res = lowres1 - lowresZ;
+                    res = lowres1 - lowres2;
                 }
 
                 if (res === 0) {
                     // Then sort by creation date
-                    res = p1.id - pZ.id;
+                    res = p1.id - p2.id;
                 }
                 return res;
             });
 
-            if (picturesPending.length === 0) {
-                // We are out of stock, make sure we reload next time
-                this._handler.markCacheDirty();
-            } else {
-                // Generate all lowres pictures
-                for (const picture of picturesPending) {
-                    // Both renderings are done in separate transactions
-                    // so that the client can see each image asap
+            // Generate all lowres pictures
+            for (const picture of picturesPending) {
+                // Both renderings are done in separate transactions
+                // so that the client can see each image asap
 
-                    // Render the lowres image if needed
-                    if (picture.status === ComputationStatus.PENDING) {
-                        await this._generatePicture(picture, false);
-                    }
+                // Render the lowres image if needed
+                if (picture.status === ComputationStatus.PENDING) {
+                    await this._generatePicture(picture, false);
+                }
 
-                    // Render the highres image if needed
-                    if (picture.highresStatus === ComputationStatus.PENDING) {
-                        await this._generatePicture(picture, true);
-                    }
+                // Render the highres image if needed
+                if (picture.highresStatus === ComputationStatus.PENDING) {
+                    await this._generatePicture(picture, true);
                 }
             }
+
             return picturesPending.length > 0;
         } catch (e) {
             console.error("Failed to process pictures");
             console.error(e);
             return false;
+        } finally {
+            // Re-schedule next
+            setTimeout(this._unqueue.bind(this), 5000);
         }
     }
 
@@ -149,16 +143,6 @@ export class Generator {
                         status: asNamed(ComputationStatus.ERROR)
                     });
                 }
-            }
-        });
-    }
-
-    protected async _scheduleNextIfNeeded(): Promise<void> {
-        return this._unqueue().then((hasGenerated: boolean) => {
-            if (this._stopOnNextTimeout) {
-                return;
-            } else {
-                setTimeout(this._scheduleNextIfNeeded.bind(this), hasGenerated ? 0 : 2000);
             }
         });
     }
