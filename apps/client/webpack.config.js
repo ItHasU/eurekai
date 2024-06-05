9b03dc2ee6cfaa73cf5396f642ac3c9b828cf7d6
Matthieu Aubry <aubry@cervval.com>
2024-06-05 22:11:42 +0200

    App manifest

--- apps/client/webpack.config.js
+++ apps/client/webpack.config.js
@@ -1,1 +1,22 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
 module.exports = require("@dagda/build/webpack-client.config")(__dirname);
+
+// -- Add manifest.json to the output --
+module.exports.plugins.push(new WebpackPwaManifest({
+    name: 'eurekAI',
+    short_name: 'eurekAI',
+    description: "Génération d'image",
+    background_color: '#ffffff',
+    crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+    icons: [
+        {
+            src: path.resolve('assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+        }
+    ],
+    inject: true,
+    ios: true,
+    publicPath: '.'
+}));
