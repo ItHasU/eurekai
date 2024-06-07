753c9ac0e6f978bd1b421525ab9abbcb597c28a8
Matthieu Aubry <aubry@cervval.com>
2024-06-07 07:11:12 +0200

    Better pwa support

--- apps/client/webpack.config.js
+++ apps/client/webpack.config.js
@@ -9,19 +9,20 @@ module.exports.plugins.push(new WebpackPwaManifest({
     short_name: 'eurekAI',
     description: "Génération d'image",
     background_color: 'rgb(43, 48, 53)',
+    theme_color: 'rgb(43, 48, 53)',
     crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
     icons: [
         {
             src: path.resolve('assets/icon.png'),
-            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+            sizes: [96, 128, 192, 256, 384, 512, 1024] // multiple sizes
+        },
+        {
+            src: path.resolve('assets/maskable_icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512, 1024],
+            purpose: 'maskable'
         }
     ],
     inject: true,
-    ios: {
-        "apple-mobile-web-app-capable": "yes",
-        "apple-mobile-web-app-status-bar-style": "black-translucent",
-        "apple-mobile-web-app-title": "eurekAI"
-    },
     publicPath: '.',
     display: "fullscreen",
 }));
